1da235097f187601254859588c7b0b8699060e83
Satoki Hata <satokihata@gmail.com>
2024-02-07 15:02:52 +0900

    add setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+
 export const App = () => {
-  return <div>App</div>;
+  const [time, setTime] = useState<string>("00:00:00");
+  return (
+    <input
+      type="time"
+      value={time}
+      step="1"
+      onChange={(e) => setTime(e.currentTarget.value)}
+    />
+  );
 };
